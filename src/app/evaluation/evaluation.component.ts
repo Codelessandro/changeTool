0f1aa43d72fbd94463f544836093061195ad84e1
Alessandro Schneider <alessandro.schneider@campus.tu-berlin.de>
2020-03-14 21:56:44 +0100

    feat: css -> scss

--- src/app/evaluation/evaluation.component.ts
+++ src/app/evaluation/evaluation.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-evaluation',
   templateUrl: './evaluation.component.html',
-  styleUrls: ['./evaluation.component.css']
+  styleUrls: ['./evaluation.component.scss']
 })
 export class EvaluationComponent implements OnInit {
 
--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -4,7 +4,7 @@ import {Question, QuestionService} from '../question.service';
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit {
 
