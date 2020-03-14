e3fbef3c08d0445985294a8059d0f6db130df39d
Alessandro Schneider <alessandro.schneider@campus.tu-berlin.de>
2020-03-14 21:18:35 +0100

    feat: evaluation

--- src/app/evaluation/evaluation.component.ts
+++ src/app/evaluation/evaluation.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EvaluationComponent implements OnInit {
 
+  value1 = 20;
+  value2 = 60;
+  value3 = 80;
+  value4 = 10;
+  value5 = 100;
+
   constructor() { }
 
   ngOnInit(): void {
