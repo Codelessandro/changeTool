26b69d5eb07c5bd0ff5af8cb0df1efcc899c7924
Alessandro Schneider <alessandro.schneider@campus.tu-berlin.de>
2020-03-14 21:19:11 +0100

    feat: question

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Question, QuestionService} from '../question.service';
 
 @Component({
   selector: 'app-question',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionComponent implements OnInit {
 
-  constructor() { }
+  question: Question;
+
+  autoTicks = false;
+  disabled = false;
+  invert = false;
+  max = 100;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
+  vertical = false;
+  tickInterval = 1;
+
+  constructor(private questionService: QuestionService) {
+    this.question = this.questionService.question(0);
+  }
 
   ngOnInit(): void {
   }
+
+  getSliderTickInterval(): number | 'auto' {
+    if (this.showTicks) {
+      return this.autoTicks ? 'auto' : this.tickInterval;
+    }
+
+    return 0;
+  }
+
+  save() {
+    this.question = this.questionService.question(1);
+  }
+
 
 }
