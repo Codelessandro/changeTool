3e8dd1e00e53ae9f73a7210b7c451067fc2ef2d7
Alessandro Schneider <alessandro.schneider@campus.tu-berlin.de>
2020-03-14 18:06:58 +0100

    feat: components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { ChooseComponent } from './choose/choose.component';
+import { QuestionComponent } from './question/question.component';
+import { EvaluationComponent } from './evaluation/evaluation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    ChooseComponent,
+    QuestionComponent,
+    EvaluationComponent
   ],
   imports: [
     BrowserModule
