05d29c341ecf6c815702d22f65034ebd67a6dae5
Alessandro Schneider <alessandro.schneider@campus.tu-berlin.de>
2020-03-20 14:56:03 +0100

    feat: abstract dimensions on choose

--- src/app/choose/choose.component.ts
+++ src/app/choose/choose.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {QuestionService} from '../question.service';
+import {Router} from '@angular/router';
+import {StateService} from '../state.service';
 
 @Component({
   selector: 'app-choose',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChooseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private stateService: StateService, private questionService: QuestionService) {
+  }
+
+
+  dimensions: any[];
 
   ngOnInit(): void {
+    this.dimensions = Array.from(new Set(this.questionService._questions.map(q => q.category)));
+    const alreadyFilledIn = Array.from(new Set(this.stateService.questionsAnswered.map(v => v[2])));
+
+    this.dimensions = this.dimensions.map(d => {
+        const is_disabled = alreadyFilledIn.includes(d)
+        return [d, is_disabled];
+      }
+    );
+    debugger;
+
+  }
+
+  goToQuestion(question: number, isDisabled: boolean) {
+    if (!isDisabled) {
+      this.router.navigate(['/question/', question]);
+    } else {
+    }
   }
 
 }
