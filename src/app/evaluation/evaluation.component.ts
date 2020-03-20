e1dfca32a9b45bfea02b47626b1abf439b4a144a
Alessandro Schneider <alessandro.schneider@campus.tu-berlin.de>
2020-03-20 14:56:31 +0100

    feat: evaluation component

--- src/app/evaluation/evaluation.component.ts
+++ src/app/evaluation/evaluation.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {QuestionService} from '../question.service';
+import {StateService} from '../state.service';
 
 @Component({
   selector: 'app-evaluation',
@@ -13,9 +16,21 @@ export class EvaluationComponent implements OnInit {
   value4 = 10;
   value5 = 100;
 
-  constructor() { }
+  constructor(private stateService: StateService, private router: Router, private questionService: QuestionService) { }
+
+  dimensions:any[];
 
   ngOnInit(): void {
+
+    this.dimensions = Array.from(new Set(this.questionService._questions.map( q => q.category )));
+
+    debugger
+    this.dimensions = this.dimensions.map( d => [d,
+      this.stateService.questionsAnswered.filter( q => q[2] === d ).map( q => q[1]).reduce( (a,b) => a+b ,0 )
+    ])
   }
+
+
+
 
 }
